4328b0e6e5e0f65a098b0dd83cb64a428cac53af
Tim <tim@attackmonkey.co.uk>
2021-06-08 14:30:23 +0100

    Initial Commit

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Turbo-Charge-Your-Content-Blocks
 This example repo shows how you can extend your content blocks in Umbraco to make your editors happy!
 
-The example site has everything in it that you needto run the examples. To log into the back office, use "Admin" and "Admin1234".
+The example site has everything in it that you needto run the examples. To log into the back office, use "test@example.com" and "Admin1234!!".
 
-All of the addditional code can be found in the App_Plugins/Skrift folder.
+All of the addditional code from the article can be found in the App_Plugins/Skrift folder.
---
+++ TurboChargeContentBlocks/TurboChargeContentBlocks/App_Plugins/Skrift/js/blocks.controller.js
@@ -0,0 +1,17 @@
+﻿angular.module("umbraco").controller("imageBlockController", function ($scope, mediaResource) {
+
+    $scope.$watch(
+        function () { return $scope.block.data.image; },
+        function () {
+            var imageUdi = $scope.block.data.image;
+            if (imageUdi) {
+                mediaResource.getById(imageUdi)
+                    .then(function (media) {
+                        $scope.imageUrl = media.mediaLink + "?width=500&upscale=false";
+                    });
+            } else {
+                $scope.imageUrl = "";
+            }
+        }
+    );
+});
